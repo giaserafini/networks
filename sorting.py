1562e478b10cb4db3a44c7a357564bf30154b1fe
giaserafini <43479060+giaserafini@users.noreply.github.com>
2019-10-28 23:52:22 +0100

    krotki komentarz sytuacyjny

--- sorting.py
+++ sorting.py
@@ -8,9 +8,11 @@ def insertion_sort(sorted_list): #rosnaco
         sorted_list[j + 1] = key
     print('\nThe sorted list: \t', sorted_list)
     print('\n')
+    print('\nThe sorted list: \t', reversed(sorted_list))
+    print('\n')
 
 lista = []
-size = int(input("\nEnter size of the list: \t"))
+size = int(input("\nEnter the size of the list: \t"))
 
 for i in range(size):
     elements = int(input("Enter the element: \t"))
@@ -22,16 +24,16 @@ def insertion_sort_d(sorted_list_d): #why you don't want to work???
     for i in range(1, len(sorted_list_d)):
         key = sorted_list_d[i]
         j=i-1
-        while j>=0 and key >sorted_list_d[j]:
+        while j>=0 and key <sorted_list_d[j]:
             sorted_list_d[j + 1] = sorted_list_d[j]
             j -=1
         sorted_list_d[j+1] = key
-    print('\nThe sorted list is: \t', sorted_list_d)
+    print('\nThe sorted list is: \t', sorted_list_d.reverse())
     print('\n')
 
 
 listad = []
-size = int(input("\nEnter size of the list: \t"))
+size = int(input("\nEnter the size of the list: \t"))
 
 for i in range(size):
     elements = int(input("Enter the element: \t"))
@@ -39,4 +41,5 @@ for i in range(size):
 
 insertion_sort(listad)
 
-
+#wiem dlaczego to nie chce sortowac na odwrót  - z jakiegos powodu wywoluje 2 razy te 1 funkcje
+#dalam w tej 1 2 wywlania i wywala jakis dziwny blad :(
