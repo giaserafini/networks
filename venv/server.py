bd6de97c7b3991a02fcec46f3d2122113d36a651
giaserafini <43479060+giaserafini@users.noreply.github.com>
2019-11-04 00:24:01 +0100

    nowa wersja

--- venv/server.py
+++ venv/server.py
@@ -3,12 +3,13 @@ import sys
 import uuid
 
 IP = "127.0.0.1"
-Port = 20001
-listeningAddress = (IP, Port)
+
 my_id = uuid.uuid1()  # identyfikator sesji
 
 UDPSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)  # gniazdo
 print ('Socket created successfully')
+Port = 20001
+listeningAddress = (IP, Port)
 UDPSocket.bind('', Port)
 #print('Socket binded to ')
 
@@ -16,23 +17,31 @@ UDPSocket.listen(5)
 print("I'm listening to you")
 
 while True:
-    #tutaj jeszcze powinno byc cos takiego tylko nwm jak to zaimplementowac, bo nwm czym jest to c czy to klient?
-    # Establish connection with client.
-   # c, addr = s.accept()
-   # print
-   # 'Got connection from', addr
+    c, addr = s.accept()
+    print ('Got connection from', addr)
+    c.send('Thank you for connecting')
+    c.close()
 
-    # send a thank you message to the client.
-    #c.send('Thank you for connecting')
 
-    # Close the connection with the client
-   # c.close()
     bytesAddressPair = UDPSocket.recvfrom(1024)
     message = bytesAddressPair[0]
     address = bytesAddressPair[1][0]
     print("Message from Client:", message)
     print("Client IP Address:", address)
+    UDPSocket.settimeout(0.5)
 
+
+BUFFER_LENGHT = 100
+
+def readUDP(UDPSocket): #odczytywanie, obsluga wyjatkow
+    try:
+        data, addr = UDPSocket.recvfrom(BUFFER_LENGHT)
+    except UDPSocket.timeout as e:
+        return b'Timeout error'
+    except Exception as e:
+        return b'Too much data'
+    else:
+        return data
 #   clientMsg = "Message from Client:{}".format(message)
 #   clientIP = "Client IP Address:{}".format(address)
 #   print(clientMsg)
